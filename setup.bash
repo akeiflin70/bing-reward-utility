5b512b8211483e9fadab28b5cc60a96c801c3d83
Alexandre <13389415+akeiflin70@users.noreply.github.com>
2023-01-29 19:50:02 +0100

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+''
+sed -i 's/Summary/'$NAME' - Summary/' /bing-rewards/BingRewards/src/rewards.py
 python /bing-rewards/BingRewards/setup.py -e e@e.e -p e -d $DISCORD_URI
 bash ./runtime.bash >/bing-rewards/BingRewards/logs/runtime.log 2>/bing-rewards/BingRewards/logs/runtime-error.log&
 echo $! > ./runtime.pid
