206a6d1e5861965c9dd666eb0957bab8678dcfc7
Alexandre <13389415+akeiflin70@users.noreply.github.com>
2023-01-29 18:59:59 +0100

    Create runtime.bash

---
+++ runtime.bash
@@ -0,0 +1,11 @@
+while :
+do
+  python /bing-rewards/BingRewards.py -nsb -di -e "$MAIL_BASE"@outlook.com -p $PASS
+  python /bing-rewards/BingRewards.py -nsb -di -e "$MAIL_BASE"4@outlook.com -p $PASS
+  python /bing-rewards/BingRewards.py -nsb -di -e "$MAIL_BASE"5@outlook.com -p $PASS
+  python /bing-rewards/BingRewards.py -nsb -di -e "$MAIL_BASE"6@outlook.com -p $PASS
+  python /bing-rewards/BingRewards.py -nsb -di -e "$MAIL_BASE"7@outlook.com -p $PASS
+  python /bing-rewards/BingRewards.py -nsb -di -e "$MAIL_BASE"8@outlook.com -p $PASS
+  python /bing-rewards/BingRewards.py -nsb -di -e "$MAIL_BASE"9@outlook.com -p $PASS
+  sleep 6h
+done
